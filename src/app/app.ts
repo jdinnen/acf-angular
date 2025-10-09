9aa5bc93547dfe37b0da8f81c259721425687009
John Dinnen <john.dinnen@gmail.com>
2025-10-08 22:16:10 -0500

    Add new theme and home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
+
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header';
+import { FooterComponent } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
